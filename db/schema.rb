d49fff855a610b0e66c61d1c5aa649e576347055
Klara Adams <klara.adams@mail.com>
2019-08-20 12:23:22 +0200

    created migration to add 'type' column to boats table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_085705) do
+ActiveRecord::Schema.define(version: 2019_08_20_102142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_08_20_085705) do
     t.integer "deck_number"
     t.integer "gallons_per_hour"
     t.string "photo"
+    t.string "type"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
